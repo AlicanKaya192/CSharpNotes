3b29058557708322dba203e39994483ecd162eed
AlicanONI.Kaya <128067349+AlicanKaya192@users.noreply.github.com>
2025-06-27 03:15:54 +0300

    Add files via upload

---
+++ Eğitim Başlangıç - 1/egitim1-WriteLine Komutları/egitim1/udemyegitim/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemyegitimWriteLineKomutları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ekrana kullanıcının görmesi için bir yazı yazdırmak istediğimizde bu komutları çağırırız. 
+
+
+            Console.Write("Merhaba Dünya ");
+
+            Console.WriteLine("Ben UMUT ");
+
+            Console.WriteLine("Ben ARDA ");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Eğitim Başlangıç - 1/egitim3-SatırSatırKodİnceleme/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bu kırmızı noktayı eklediğimizde kod o kısma geldiği an durur. Ben o satırı atlayana 
+            // kadar kod çalışmaz veya devam etmez.
+
+            Console.WriteLine("1.Satır"); 
+            Console.WriteLine("2.Satır");
+            Console.WriteLine("3.Satır");
+            Console.WriteLine("4.Satır");
+            Console.WriteLine("5.Satır");
+            Console.ReadLine();
+        }
+    }
+}
